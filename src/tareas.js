e95e731b2893b7dcfb696a75e377ef8d20f65350
Matías Gemino <perrodeforme@hotmail.com>
2024-03-18 21:44:12 -0300

    Los primeros cambios

---
+++ src/tareas.js
@@ -0,0 +1,21 @@
+class GestorTareas {
+    constructor() {
+        this.tareas = [];
+    }
+
+    agregarTarea(tarea) {
+        if (tarea.trim() !== '') {
+            this.tareas.push({
+                tarea: tarea,
+                completada: false
+            });
+            console.log('Nueva tarea agregada:', tarea);
+        } else {
+            console.error('La tarea no puede estar vacía');
+        }
+    }
+
+    obtenerTareas() {
+        return this.tareas;
+    }
+}
